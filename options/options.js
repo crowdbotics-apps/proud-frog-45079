1959b246ff6cc830cc1bdf820a6078da50085000
Crowdbotics <team@crowdbotics.com>
2023-12-18 15:44:33 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "proud-frog-45079",
+  url: "https://proud-frog-45079.botics.co",
+  api: "https://proud-frog-45079.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
